13377bf5357ad24b9491506a75fc926d30e6c537
Hidde Jense <JenseH@cardiff.ac.uk>
2024-07-23 09:28:16 -0500

    PEP8 formatting.

--- cosmopower/__main__.py
+++ cosmopower/__main__.py
@@ -5,28 +5,31 @@ from importlib import import_module
 #    cosmopower cmd [args]
 # will invoke the function func in module.py with args as its arguments.
 commands = {
-    "validate" : ("validate", "validate_yaml"),
-    "generate" : ("spectra", "generate_spectra"),
-    "train" : ("train", "train_networks"),
-    "show-training" : ("train", "show_training"),
-    "show-validation" : ("validate", "show_validation"),
+    "validate": ("validate", "validate_yaml"),
+    "generate": ("spectra", "generate_spectra"),
+    "train": ("train", "train_networks"),
+    "show-training": ("train", "show_training"),
+    "show-validation": ("validate", "show_validation"),
 }
 
-helpmsg = "Command '{0:s}' not known. Try one of:\n\t" + ", ".join(sorted(commands.keys()))
+helpmsg = "Command '{0:s}' not known. Try one of:\n\t" + \
+          ", ".join(sorted(commands.keys()))
 
 if __name__ == "__main__":
     try:
         cmd = sys.argv[1].lower()
     except IndexError:
-        print("Try:\n\tcosmopower <command> -h\nWhere <command> is one of " + ", ".join(sorted(commands.keys())))
+        print("Try:\n\tcosmopower <command> -h\nWhere <command> is one of " +
+              ", ".join(sorted(commands.keys())))
         exit()
     else:
         if cmd == "help":
-            print("Try:\n\tcosmopower <command> -h\nWhere <command> is one of " + ", ".join(sorted(commands.keys())))
+            print("Try:\n\tcosmopower <command> -h\nWhere <command> is \
+                   one of " + ", ".join(sorted(commands.keys())))
             exit()
-        
+
         module, func = commands.get(cmd, (None, None))
-        
+
         if func is not None:
             getattr(import_module("cosmopower." + module), func)(sys.argv[2:])
         else:
--- pickle_to_npz.py
+++ pickle_to_npz.py
@@ -4,49 +4,57 @@ import argparse
 import os
 import sys
 
-argp = argparse.ArgumentParser(description = "Re-format old cosmopower Pickle files to npz files.")
-argp.add_argument("filename", type = str, help = "The path to the .pkl file (without extension).")
-argp.add_argument("-p", "--pca-nn", dest = "PCAplusNN", action = "store_true", help = "The network is a PCA+NN type network.")
+argp = argparse.ArgumentParser(description="Re-format old cosmopower Pickle \
+                                              files to npz files.")
+argp.add_argument("filename", type=str,
+                  help="The path to the .pkl file (without extension).")
+argp.add_argument("-p", "--pca-nn", dest="PCAplusNN", action="store_true",
+                  help="The network is a PCA+NN type network.")
 args = argp.parse_args()
 
 fn, ext = os.path.splitext(args.filename)
 if ext != "":
-	print("Provide the filename WITHOUT the extension!")
-	sys.exit()
+    print("Provide the filename WITHOUT the extension!")
+    sys.exit()
 
 with open(fn + ".pkl", "rb") as fp:
-	if args.PCAplusNN:
-		W, b, alphas, betas, parameters_mean, parameters_std, pca_mean, pca_std, features_mean, features_std, parameters, n_parameters, modes, n_modes, n_pcas, pca_transform_matrix, n_hidden, n_layers, architecture = pickle.load(fp)
-	else:
-		W, b, alphas, betas, parameters_mean, parameters_std, features_mean, features_std, n_parameters, parameters, n_modes, modes, n_hidden, n_layers, architecture = pickle.load(fp)
-	
-	attributes = { }
-	attributes["architecture"] = architecture
-	attributes["n_layers"] = n_layers
-	attributes["n_hidden"] = n_hidden
-	attributes["n_parameters"] = n_parameters
-	attributes["n_modes"] = n_modes
+    if args.PCAplusNN:
+        W, b, alphas, betas, parameters_mean, parameters_std, pca_mean, \
+        pca_std, features_mean, features_std, parameters, n_parameters, \
+        modes, n_modes, n_pcas, pca_transform_matrix, n_hidden, n_layers, \
+        architecture = pickle.load(fp)
+    else:
+        W, b, alphas, betas, parameters_mean, parameters_std, features_mean, \
+        features_std, n_parameters, parameters, n_modes, modes, n_hidden, \
+        n_layers, architecture = pickle.load(fp)
 
-	attributes["parameters"] = parameters
-	attributes["modes"] = modes
+    attributes = {}
+    attributes["architecture"] = architecture
+    attributes["n_layers"] = n_layers
+    attributes["n_hidden"] = n_hidden
+    attributes["n_parameters"] = n_parameters
+    attributes["n_modes"] = n_modes
 
-	attributes["parameters_mean"] = parameters_mean
-	attributes["parameters_std"] = parameters_std
-	attributes["features_mean"] = features_mean
-	attributes["features_std"] = features_std
+    attributes["parameters"] = parameters
+    attributes["modes"] = modes
 
-	for i in range(n_layers):
-		attributes[f"W_{i}"] = W[i]
-		attributes[f"b_{i}"] = b[i]
-	for i in range(n_layers-1):
-		attributes[f"alphas_{i}"] = alphas[i]
-		attributes[f"betas_{i}"] = betas[i]
-	
-	if args.PCAplusNN:
-		attributes["pca_mean"] = pca_mean
-		attributes["pca_std"] = pca_std
-		attributes["n_pcas"] = n_pcas
-		attributes["pca_transform_matrix"] = pca_transform_matrix
+    attributes["parameters_mean"] = parameters_mean
+    attributes["parameters_std"] = parameters_std
+    attributes["features_mean"] = features_mean
+    attributes["features_std"] = features_std
+
+    for i in range(n_layers):
+        attributes[f"W_{i}"] = W[i]
+        attributes[f"b_{i}"] = b[i]
+    for i in range(n_layers-1):
+        attributes[f"alphas_{i}"] = alphas[i]
+        attributes[f"betas_{i}"] = betas[i]
+
+    if args.PCAplusNN:
+        attributes["pca_mean"] = pca_mean
+        attributes["pca_std"] = pca_std
+        attributes["n_pcas"] = n_pcas
+        attributes["pca_transform_matrix"] = pca_transform_matrix
 
 with open(fn + ".npz", "wb") as fp:
-	np.savez_compressed(fp, **attributes)
+    np.savez_compressed(fp, **attributes)
